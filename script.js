647d47999be0bd8de5b2e2c6f0cddb411f5ad63a
alexmiller522 <32317337+alexmiller522@users.noreply.github.com>
2021-11-06 18:05:32 -0400

    Updated changes
    adding a pic and trying a get function

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
 function searchResults(Name) {
-    
+
 }
+
+function searchButton() {
+    Result = httpGet('https://projects.propublica.org/nonprofits/api/v2/search.json?q=propublica')
+    document.getElementById('resultsInformation').innerHTML = Result;
+
+}
+
+var url = "https://projects.propublica.org/nonprofits/api/v2/search.json?state%5Bid%5D=TX";
+
+var xhr = new XMLHttpRequest();
+xhr.open("GET", url);
+
+xhr.onreadystatechange = function () {
+   if (xhr.readyState === 4) {
+      console.log(xhr.status);
+      console.log(xhr.responseText);
+   }};
+
+xhr.send();
