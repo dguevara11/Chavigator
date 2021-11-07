945d264c6a516e80c5c6f02cf5524db681c77537
alexmiller522 <32317337+alexmiller522@users.noreply.github.com>
2021-11-06 21:02:02 -0400

    Test get

--- script.js
+++ script.js
@@ -1,22 +1,86 @@
-function searchResults(Name) {
+// function searchResults(Name) {
 
-}
+// }
 
 function searchButton() {
-    Result = httpGet('https://projects.propublica.org/nonprofits/api/v2/search.json?q=propublica')
-    document.getElementById('resultsInformation').innerHTML = Result;
+    //Result = httpGet('https://projects.propublica.org/nonprofits/api/v2/search.json?q=propublica')
+    // document.getElementById('resultsInformation').innerHTML = Result;
 
 }
 
-var url = "https://projects.propublica.org/nonprofits/api/v2/search.json?state%5Bid%5D=TX";
 
-var xhr = new XMLHttpRequest();
-xhr.open("GET", url);
+// (function() {
+//     var httpRequest;
+//     document.getElementById("searchButton").addEventListener('click', makeRequest);
+  
+//     function makeRequest() {
+//       httpRequest = new XMLHttpRequest();
+  
+//       if (!httpRequest) {
+//         alert('Giving up :( Cannot create an XMLHTTP instance');
+//         return false;
+//       }
+//       httpRequest.onreadystatechange = alertContents;
+//       httpRequest.open('GET', 'https://projects.propublica.org/nonprofits/api/v2/search.json?state%5Bid%5D=TX');
+//       httpRequest.send();
+//     }
+  
+//     function alertContents() {
+//       if (httpRequest.readyState === XMLHttpRequest.DONE) {
+//         if (httpRequest.status === 200) {
+//           alert(httpRequest.responseText);
+//         } else {
+//           alert('There was a problem with the request.');
+//         }
+//       }
+//     }
+//   })();
 
-xhr.onreadystatechange = function () {
-   if (xhr.readyState === 4) {
-      console.log(xhr.status);
-      console.log(xhr.responseText);
-   }};
+//https://projects.propublica.org/nonprofits/api/v2/search.json?state%5Bid%5D=TX
 
-xhr.send();
+
+
+// function httpGet(theUrl)
+// {
+//     var xmlHttp = new XMLHttpRequest();
+//     xmlHttp.open( "GET", theUrl, true ); 
+//     xmlHttp.send( null );
+//     return xmlHttp.responseText;
+// }
+
+
+$.ajax({
+    "headers": {
+        "Access-Control-Allow-Origin":"*"
+    },
+    url: "https://projects.propublica.org/nonprofits/api/v2/search.json?state%5Bid%5D=TX",
+    type: 'GET',
+    dataType: 'json', // added data type
+    success: function(res) {
+        console.log(res);
+        alert(res);
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
